c63dd411c7013c75d5fe53e8cfbbcbb9e70ebd94
krishna-jha-25 <krishna@torinit.ca>
2023-02-10 18:39:39 +0530

    minor change

--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -78,7 +78,7 @@ export default function EventList() {
     );
 
     const handleSendBulkRequests = async () => {
-        if(!selectedRequests) {
+        if(!selectedRequests?.requests?.length) {
             return
         }
         const response = await fetch(
